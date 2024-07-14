76987007a676de364c569ed566ce6e2daebd6a62
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-07-15 00:40:29 +0500

    second push

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logo from "./assets/imgs/logo.svg";
+import searchIcon from "./assets/imgs/searchIcon.svg";
+import earthIcon from "./assets/imgs/earthIcon.svg"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World</h1>
+    <header className='header'>
+      <section className='header__one'>
+        <div className="container header__one__container">
+          <img src={logo} className='logo' alt="logo.svg" />
+          <div className="search">
+            <img src={searchIcon} alt="searchIcon.svg" className="searchIcon" />
+            <input type="search" name="searchText" id="searchText" placeholder='Cari apapun disini...' />
+          </div>
+          <div className="earth">
+            <img src={earthIcon} alt="earthIcon.svg" />
+            <select name="address1" id="address1">
+              <option value="">Bahasa Indonesia</option>
+            </select>
+          </div>
+        </div>
+      </section>
+    </header>
+
     </>
   )
 }
